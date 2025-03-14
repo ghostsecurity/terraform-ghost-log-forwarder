c1b1191e1198d3b6ea115ceed7e0be0bc3d870d8
David Matslofva <david.matslofva@ghost.security>
2025-03-14 14:15:52 +0100

    GHO-9342: Trigger on all ObjectCreated events

--- log_conversion.tf
+++ log_conversion.tf
@@ -94,13 +94,13 @@ resource "aws_s3_bucket_notification" "input" {
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.log_converter.arn
-    events              = ["s3:ObjectCreated:Put"]
+    events              = ["s3:ObjectCreated:*"]
     filter_suffix       = ".gz"
   }
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.log_converter.arn
-    events              = ["s3:ObjectCreated:Put"]
+    events              = ["s3:ObjectCreated:*"]
     filter_suffix       = ".log"
   }
 
