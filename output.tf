8ae77c51d93524b82174ac6838fe8fdc1c72ea6f
Brad Geesaman <3769609+bgeesaman@users.noreply.github.com>
2025-03-24 12:48:22 -0400

    add lambda function and role arns as module outputs

--- output.tf
+++ output.tf
@@ -2,3 +2,13 @@ output "s3_input_bucket" {
   description = "The name of the input bucket"
   value       = aws_s3_bucket.input_bucket.id
 }
+
+output "lambda_role_arn" {
+  description = "The ARN of the IAM Role used by the Log Processing Lambda Function to access Secrets and S3"
+  value       = aws_iam_role.log_converter_role.arn
+}
+
+output "lambda_arn" {
+  description = "The ARN of the Log Processing Lambda Function"
+  value       = aws_lambda_function.log_converter.arn
+}
